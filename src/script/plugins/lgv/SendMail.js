3cebc8765dcd373f884d9643e5763595d2599605
tegtmesa <tegtmesa@LGV-W-D556430.fhhnet.stadt.hamburg.de>
2013-09-16 17:10:32 +0200

    Error Fix

--- src/script/plugins/lgv/SendMail.js
+++ src/script/plugins/lgv/SendMail.js
@@ -51,48 +51,6 @@ lgv.plugins.SendMail = Ext.extend(gxp.plugins.Tool, {
         }
     });
     
-    
-    /*
-     * Diese Funktion fügt eine CSS-Klasse für die zu erstellen Aktion hinzu, in der das Bild für die Toolbar definiert wird
-     * 
-     * Es wird erst ein neuer Dokumenten-Style erzeugt und dieser dem DOM hinzugefügt.
-     * im try...catch-Block wird eine Browser-Weiche abgefragt.
-     * cssRules und "insertRule" für Firefox, rules und "addRule" für IE
-     * leider kann Firefox die cssRules nicht sofort nach Erzeugung erreichen, deswegen der Timeout im Catch-Block
-     * 
-     * Parameter firstTime, damit keine Endlosschleife entsteht, falls Probleme beim Erzeugen der neuen Styles auftreten.
-     * 
-     */
-    function _addCSS_Class(firstTime){
-    	
-		if( document.styleSheets ) {
-		    var myStyle = document.createElement("style");
-		    myStyle.setAttribute( "type", "text/css" );
-		    document.getElementsByTagName("head")[0].appendChild(myStyle);
-		 
-		    var styles = document.styleSheets.length;
-		    myStyle = document.styleSheets[styles-1];
-		    
-		    try{
-		 		// cssRules wird von Firefox verwendet, rules vom IE
-		 	    if( document.styleSheets[0].cssRules ) {
-			        myStyle.insertRule(".mailtoActionCSS { background-image: url(img/email.png); }", 0);
-			    }
-			    else if ( document.styleSheets[0].rules ) {
-					myStyle.addRule(".mailtoActionCSS", "background-image: url(img/email.png);");
-				}
-			}
-			catch(ex){
-				if(firstTime){
-					window.setTimeout(_addCSS_Class, 1000);
-				}					
-			}	
-		}
-	}
-    
-    // DOM-ready-Event
-	//Ext.onReady(_addCSS_Class(true));
-    
     return lgv.plugins.SendMail.superclass.addActions.call(this, [action]);
 
   }
